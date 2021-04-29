7a62f09987982b29c3c907804d7620e3d8b639ba
Iizuki <36295229+Iizuki@users.noreply.github.com>
2021-04-29 15:29:58 +0300

    Fixed applied gitignore

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
-import qualified MyLib (someFunc)
-
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  putStrLn "Welcome to BlackJack Trainer!"
+  decks <- getNumberOfDecks
+  putStrLn "Bye bye!"
+
+getNumberOfDecks :: IO Int
+getNumberOfDecks = do
+    putStrLn "How many deck's would you like to play with? (1-8)"
+    input <- getLine 
+    let numberOfDecks = (read input :: Int)
+    if 0 < numberOfDecks && numberOfDecks < 9
+        then return numberOfDecks
+    else getNumberOfDecks
